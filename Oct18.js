d96455dbc41ac02f7fc4fdb58de76a3b9d6b040e
carter dooley <carter.j.dooley43@gmail.com>
2021-10-18 09:25:53 -0500

    First Version

--- Oct18.js
+++ Oct18.js
@@ -1,1 +1,13 @@
-//code
+let myArray = [-3,-1,0,0,1,2,3]
+let x = 0
+while (x < myArray.length) {
+for (i = 0; i < myArray.length; i++){
+    if (myArray[i] + myArray[x] === 0){
+        console.log('True')
+        i += 10
+        x += 10
+    } else {
+        console.log('False')
+} x++
+}
+}
