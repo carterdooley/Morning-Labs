2f5db6b8cbd02aeb09cda3e599f67f36deeb3b78
Carter Dooley <carter.j.dooley43@gmail.com>
2021-10-21 09:32:04 -0500

    Finished Oct21

--- Oct21.js
+++ Oct21.js
@@ -1,8 +1,23 @@
 //code
 
+// const isPalindrom = str => {
+//     return str == str.split('').reverse().join('')
+// }
+
+
+// console.log(isPalindrom("hello"))
+
+
+let forward = " "
+
 const isPalindrom = str => {
-    return str == str.split('').reverse().join('')
+ if (str == str.split('').reverse().join('')) {
+     return true
+ } else {
+     return false
+ }
 }
 
 
 console.log(isPalindrom("hello"))
+
