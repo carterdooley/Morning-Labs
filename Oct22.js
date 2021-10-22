d4ccecf9cce56247e429605d4771a3908b67bb2c
carter dooley <carter.j.dooley43@gmail.com>
2021-10-22 12:41:00 -0500

    Almost Done

--- Oct22.js
+++ Oct22.js
@@ -1,1 +1,20 @@
-//code
+//code\
+
+
+const pigLatin = (str) => {
+    str = str.split(' ')
+    for (i = 0; i <str.length; i++) {
+        if (str[i].startsWith('a','e','i' ,'o' ,'u')) {
+            str[i] += 'yay'
+            return str
+        } else {
+            newStr = str[i][0]
+            str[i] = str[i].replace(str[i][0], '')
+            str[i] += newStr
+            return str[i] += 'ay'
+
+        }
+    }
+}
+
+console.log(pigLatin('hello apple'))
