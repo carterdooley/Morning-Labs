2eed78474d2b77c52ff99ee1f6a4236497835eea
carter dooley <carter.j.dooley43@gmail.com>
2021-10-22 15:36:01 -0500

    Message

--- Oct22.js
+++ Oct22.js
@@ -62,7 +62,7 @@ Didn't make sense not to live for fun
 Your brain gets smart but your head gets dumb
 So much to do, so much to see
 So what's wrong with taking the back streets?
-You'll never know if you don't go (go!)
+You'll never know if you don't go go!
 You'll never shine if you don't glow
 Hey now, you're an all-star, get your game on, go play
 Hey now, you're a rock star, get the show on, get paid
@@ -70,3 +70,12 @@ And all that glitters is gold
 Only shooting stars break the mold
 And all that glitters is gold
 Only shooting stars break the mold`))
+
+
+
+
+//Sorry, I couldn't help but try.
+
+
+
+//"Allstar" by Smash Mouth lyrics from https://www.google.com/search?q=all+star+lyrics&rlz=1C1ONGR_enUS967US967&sxsrf=AOaemvJo75G6cwwd8rcqmtTjnRq8-1M6zw%3A1634934144414&ei=gB1zYei4GIapqtsPpfapSA&oq=all+star+lyrics&gs_lcp=Cgdnd3Mtd2l6EAEYADIGCAAQChBDMgYILhAKEEMyBQgAEIAEMgsILhCABBDHARCvATILCC4QgAQQxwEQrwEyCwguEIAEEMcBEK8BMgsILhCABBDHARCvATILCC4QgAQQxwEQrwEyBwgAEIAEEAoyCwguEIAEEMcBEK8BOgQIIxAnOgUIABCRAjoLCC4QgAQQxwEQowI6CAguELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQsQMQgwE6DgguEIAEELEDEMcBEKMCOgQIABBDOgQILhBDOggILhCABBCxAzoHCC4QsQMQQzoICAAQgAQQsQM6BwgAELEDEENKBAhBGABQniVY9kFg3lJoAHABeACAAYABiAGqBpIBAzQuNJgBAKABAcABAQ&sclient=gws-wiz
