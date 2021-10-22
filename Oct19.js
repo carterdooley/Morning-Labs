fc48f55d63c6e85d8fe1d75ca5cab8ed31e544fa
carter dooley <carter.j.dooley43@gmail.com>
2021-10-22 12:02:56 -0500

    Create Oct22

--- Oct19.js
+++ Oct19.js
@@ -4,7 +4,7 @@ let n = []
 
 function luckyNumbers (num){
     for (i = 0; i < num; i++) {
-        n.push(Math.floor(Math.random(num) * 9));
+        n.push(Math.floor(Math.random(num) * 10)+1);
         if (n[i] > 0){
             console.log(n)
         } 
