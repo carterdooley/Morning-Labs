f36ec830c2e16822005237ac04407f4bcddc3121
carter dooley <carter.j.dooley43@gmail.com>
2021-10-22 14:59:14 -0500

    Finished Oct22

--- Oct22.js
+++ Oct22.js
@@ -2,19 +2,21 @@
 
 
 const pigLatin = (str) => {
+    str = str.toLowerCase()
+    str = str.trim()
     str = str.split(' ')
-    for (i = 0; i <str.length; i++) {
+    for (i = 0; i < str.length; i++) {
         if (str[i].startsWith('a','e','i' ,'o' ,'u')) {
             str[i] += 'yay'
-            return str
+ 
         } else {
             newStr = str[i][0]
             str[i] = str[i].replace(str[i][0], '')
             str[i] += newStr
-            return str[i] += 'ay'
-
+            str[i] += 'ay'
         }
-    }
+    } str = str.join(' ')
+    return str
 }
 
-console.log(pigLatin('hello apple'))
+console.log(pigLatin('hello I am Carter'))
