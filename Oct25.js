811fdbbbe049bd3f218ca15a80429881ed36bb77
carter dooley <carter.j.dooley43@gmail.com>
2021-10-25 09:44:51 -0500

    Finished

--- Oct25.js
+++ Oct25.js
@@ -1,1 +1,22 @@
 //code
+
+let vowels = 0
+let consenants = 0
+
+const hasMoreVowels = (word) => {
+    word.toLowerCase()
+    word.split()
+    for (i = 0; i < word.length; i++) {
+        if("aieou".includes(word[i])){
+            vowels += 1
+        } else {
+            consenants += 1
+        }
+    } return vowels > consenants
+}
+
+
+
+console.log(hasMoreVowels('duck'))
+
+// const hasMoreVowels1 = word => word.match(/aeiouAEIOU/)
