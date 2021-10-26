96acf1fcb397341cad4cc56f391650b70a9ed635
carter dooley <carter.j.dooley43@gmail.com>
2021-10-26 09:56:24 -0500

    Finished Oct26

--- Oct26.js
+++ Oct26.js
@@ -1,1 +1,27 @@
 //code
+
+let largest = 0
+let smallest = 0
+
+const best = numArr => {
+    for (i = 1; i < numArr.length; i++) {
+        if (numArr[i] > numArr[largest]) {
+            largest = i
+        }
+    }
+    newArr = numArr.slice(0,largest)
+
+    let profit = (numArr[largest] - Math.min.apply(null, newArr))
+    if (profit < 0) {
+        profit = 0;
+    } 
+
+
+    return profit
+}
+
+
+   
+
+
+console.log(best([2,4,7,8,1]))
