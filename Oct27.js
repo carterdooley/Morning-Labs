2ebbfabf45f08037c1abb3048d1bb13cd5f0764c
carter dooley <carter.j.dooley43@gmail.com>
2021-10-27 09:57:37 -0500

    Why Undefined

--- Oct27.js
+++ Oct27.js
@@ -1,1 +1,18 @@
 //code
+
+let num = 0
+let newArr = ''
+
+const decoder = code => {
+    for (i = 0; i < code.length; i++) {
+        if (code[i].match(/^[0-9]*$/)) {
+            num = code[i]
+            i += num
+            console.log(code[i])
+        }
+    }
+}
+
+
+decoder('dj2hfkgy')
+
