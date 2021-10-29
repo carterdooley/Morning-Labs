38bc35614fc44fac6f8ae73748b2caf00724379a
carter dooley <carter.j.dooley43@gmail.com>
2021-10-29 11:09:42 -0500

    Init

--- Oct28.js
+++ Oct28.js
@@ -1,1 +1,14 @@
-//Code
+let b, result = 0
+
+const printDigits = num => {
+    while (num > 0) {
+        b = num % 10
+        result = (result * 10) + b
+        num = Math.floor(num/10)
+        console.log(b)
+    } 
+}
+
+
+
+printDigits(539)
