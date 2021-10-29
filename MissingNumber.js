543544a2e6fc7d4837c12915efe3e6fc7bcd3326
carter dooley <carter.j.dooley43@gmail.com>
2021-10-29 11:40:59 -0500

    Finished

--- MissingNumber.js
+++ MissingNumber.js
@@ -1,1 +1,11 @@
-//code
+
+let missinNum = (numArr, maxNum) => {
+    for (i = 1; i < maxNum; i++) {
+        if ((numArr[i] + 1) !== numArr[i + 1]) {
+            return numArr[i] + 1
+        }
+    }
+}
+
+
+console.log(missinNum([1, 2, 3, 4, 6, 7, 8, 9, 10], 10))
