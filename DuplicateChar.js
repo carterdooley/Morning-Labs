0e4fa47dc866d7fe96435dd6e102c437bdb90d4d
carter dooley <carter.j.dooley43@gmail.com>
2021-11-02 10:01:48 -0500

    Something

--- DuplicateChar.js
+++ DuplicateChar.js
@@ -1,2 +1,19 @@
 //Cide
 
+const unique = (str) => {
+    for(i=0; i < str.length; i++) {
+        // let j = str.charCodeAt(i)
+        for(x=1; x < str.length; x++){
+            let k = str.charCodeAt(x)
+            let j = str.charCodeAt(i)
+            console.log(j)
+            console.log(k)
+            if (j === k && x !== i) {
+                str = str.substr(x-1)
+                console.log(str)
+            }
+        } 
+    } 
+}
+
+unique('hello world')
